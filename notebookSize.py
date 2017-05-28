ed8d7c1913e6c8331b79f5e1a3c09af905198e2a
khw7096 <khw7096@gmail.com>
2017-05-28 19:50:12 +0900

    노트북 사이즈 구하기

---
+++ notebookSize.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#coding:utf-8
+# pepper는 1.4kg 미만의 노트북 모델을 타겟으로 한다.
+MODELS = []
+
+# 리스트의 순서 : 회사, 모델명, 가로mm, 세로mm, 높이mm, 무게kg
+# 삼성
+MODELS.append(["samsung", "NT500R4P-LD1S", 336.0, 230.5, 20.9, 1.65])
+MODELS.append(["samsung", "NT900X3H-K38L", 318.8, 218.5, 13.4, 0.86])
+MODELS.append(["samsung", "NT900X5J-K28S", 346.5, 226.8, 14.5, 1.29])
+
+# 레노보
+MODELS.append(["lenovo", "X1-CARBON", 323.5, 217.1, 15.95, 1.13])
+
+# 애플
+MODELS.append(["apple", "macbook-pro13", 304.1, 212.4, 14.9, 1.37])
+
+def averageSize():
+	"""
+	각 모델의 평균사이즈를 구하는 함수이다.
+	"""
+	width = 0.0
+	height = 0.0
+	depth = 0.0
+	total = 0
+
+	for m in MODELS:
+		if m[5] > 1.4: # pepper 프로젝트는 1.4킬로그램 미만의 노트북만 취급한다.
+			continue
+		width += m[2]
+		height += m[3]
+		depth += m[4]
+		total += 1
+	print("Average  Width : %.1f" % ( width / total))
+	print("Average Height : %.1f" % ( height / total))
+	print("Average  Depth : %.1f" % ( depth / total))
+
+def main():
+	averageSize()
+
+if __name__ == "__main__":
+	main()
